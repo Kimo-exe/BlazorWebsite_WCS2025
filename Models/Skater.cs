3cd31e9628d828890310ab41d4edeb202fcb9e93
Akram <akram.efm.sobh@gmail.com>
2025-12-19 11:52:12 +0100

    first commit

---
+++ Models/Skater.cs
@@ -0,0 +1,31 @@
+﻿namespace Blazor.Models
+{
+    public class Skater
+    {
+        public string Naam { get; set; }
+
+        public string Land { get; set; }
+
+        public int Leeftijd { get; set; }
+
+        public string Stance { get; set; }
+
+        public int WorldRanking { get; set; }
+
+        public string Flag { get; set; }
+        
+        public string Picture { get; set; }
+
+        public Skater(string name, string country, int age, string stance, int rank, string flag, string pic)
+        {
+            Naam = name;
+            Land = country;
+            Leeftijd = age;
+            Stance = stance;
+            WorldRanking = rank;
+            Flag = flag;
+            Picture = pic;
+        }
+
+    }
+}
---
+++ Pages/Quiz.cs
@@ -0,0 +1,17 @@
+﻿namespace Blazor.Models
+{
+    public class Quiz
+    {
+        public string QuestionText { get; set; }
+        public List<string> Options { get; set; }
+        public bool Answer { get; set; }
+        public int QuestionCounter { get; set; }
+
+        public int AddScore(int counter)
+        {
+            counter++;
+
+            return counter;
+        }
+    }
+}
